7e32d3bcae81af48d53d62dc5e711e61d7b091b9
Your Name <you@example.com>
2020-10-07 08:41:21 +0700

    code

---
+++ app/src/main/java/com/nghia/pvn/bt4/Login.java
@@ -0,0 +1,14 @@
+package com.nghia.pvn.bt4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/nghia/pvn/bt4/activity_2.java
@@ -0,0 +1,16 @@
+package com.nghia.pvn.bt4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class activity_2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+    }
+}
